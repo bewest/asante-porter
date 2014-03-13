580f13bb8da310b329e20d5af93515e9d3c1a9f3
Ben West <bewest@gmail.com>
2014-03-13 09:23:44 -0700

    WIP: stub out use of npm module binary
    Verified this approach in conduit-chrome.

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
 
 var com = require("serialport");
+var binary = require('binary');
+var es = require('event-stream');
+var put = require('put');
+
+function protocol ( ) {
+  function dispatch (vars) {
+    console.log('dispatch', vars);
+    if (vars.code == 0x05) {
+      stream.emit('beacon', vars);
+    } else {
+      stream.emit('response', vars);
+    }
+    return this;
+  }
+
+  var known = new Buffer([0x7E, 0x05, 0, 0, 0x1F, 0x8F]);
+  var stream = message.call(binary( ))
+    .tap(dispatch)
+    .loop(function onBeacon (end, vars) {
+      message.call(this).tap(dispatch);
+    })
+  ;
+  function message ( ) {
+    console.log('incoming message');
+    var self = this;
+    return self
+      .word8u('sync')
+      .word8u('code')
+      .word8u('length')
+      .tap(function (vars) {
+        if (vars.sync == 0x7E) {
+          this
+            .into('payload', function ( ) {
+              this
+              .buffer('body', vars.length === 0 ? 1 : vars.length)
+            })
+            .word8u('crc0')
+            .word8u('crc1')
+        }
+      })
+      ;
+  }
+  return stream;
+
+}
+
+module.exports = protocol;
 
 if (!module.parent) {
   console.log('Howdy', "let's talk to an insulin pump!");
