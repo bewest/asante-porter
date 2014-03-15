f3bfb6e49cf0fe44be1edb770e6ef4e43a0c1ca1
Ben West <bewest@gmail.com>
2014-03-15 12:49:36 -0700

    fix response parsing
    This fixes issues handling the payload and the length correctly.
    The format is actually LSB, MSB for length, using the correct logic
    here obviates the special casing.
    
    Also, make the driver a duplex stream.

--- index.js
+++ index.js
@@ -12,10 +12,16 @@ if (!module.parent) {
   // should beacon at first
   serial.open(function (err) {
     console.log('opened', arguments);
-    var driver = protocol( );
+    var opts = { log: log };
+    var driver = protocol(opts);
     serial.pipe(driver);
+    var count = 0;
     driver.on('beacon', function (vars) {
       log.info('beacon', vars);
+      if (++count > 3) {
+        log.info('sending query');
+        driver.query( ).write(serial);
+      }
     });
     driver.on('response', function (vars) {
       log.info('response', vars);
